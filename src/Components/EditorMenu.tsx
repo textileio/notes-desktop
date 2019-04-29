f4ce288b99cdd882d70fe0a4171d6e39efccfda8
andrew <andrewxhill@gmail.com>
2019-04-29 13:46:30 -0700

    adds new files

---
+++ src/Components/EditorMenu.tsx
@@ -0,0 +1,185 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Icons from '@material-ui/icons'
+import styled from '@emotion/styled'
+
+export const Menu = styled('div')`
+  & > * {
+    display: inline-block;
+  }
+  & > * + * {
+    margin-left: 15px;
+  }
+`
+const StyledMenu = styled(Menu)`
+  position: absolute;
+  z-index: 1000;
+  top: -10000px;
+  left: -10000px;
+  opacity: 0;
+  background-color: #fff;
+  border: 1px solid #AAA;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: row;
+  align-content: space-between;
+  justify-content: space-between;
+  height: 184px;
+  transition: opacity 0.75s;
+`
+
+interface EditorMenuProps {
+  editor: any
+  innerRef: (ref: HTMLDivElement | null) => void
+  style?: React.CSSProperties
+}
+class EditorMenu extends React.Component<EditorMenuProps> {
+
+ onClickMark = (event: any, type: string) => {
+   const { editor } = this.props
+   event.preventDefault()
+   editor.toggleMark(type)
+ }
+
+ getIcon = (icon: string, isActive?: boolean, onMouseDown?: (event: React.MouseEvent) => void) => {
+   const iconStyle: React.CSSProperties = {
+     border: isActive ? '1px solid rgba(0,0,0,0.2)' : '1px solid #FFF',
+     borderRadius: 4,
+     padding: 10,
+     margin: 2,
+     fontSize: 20,
+     color: 'black',
+     zIndex: 10001
+   }
+   switch(icon) {
+     case 'format_heading_one': 
+       return (
+         <Icons.FormatSize style={iconStyle} onMouseDown={onMouseDown}/>
+       )
+      case 'format_heading_two': 
+        return (
+          <Icons.FormatSize style={{...iconStyle, fontSize: 14, lineHeight: 20}} onMouseDown={onMouseDown}/>
+        )
+     case 'format_bold': 
+       return (
+         <Icons.FormatBold style={iconStyle} onMouseDown={onMouseDown}/>
+       )
+     case 'format_italic': 
+       return (
+         <Icons.FormatItalic style={iconStyle}  onMouseDown={onMouseDown}/>
+       )
+     case 'format_underlined': 
+       return (
+         <Icons.FormatUnderlined style={iconStyle} onMouseDown={onMouseDown} />
+       )
+     case 'code': 
+       return (
+         <Icons.Code style={iconStyle} onMouseDown={onMouseDown} />
+       )
+     case 'format_list_numbered': 
+       return (
+         <Icons.FormatListNumbered style={iconStyle} onMouseDown={onMouseDown} />
+       )
+   }
+ }
+ renderMarkButton = (type: string, icon: string) => {
+   const { editor } = this.props
+   const { value } = editor
+   const isActive = value.activeMarks.some((mark: any) => mark.type === type)
+   return this.getIcon(icon, isActive, (event) => { this.onClickMark(event, type) })
+ }
+
+ onClickBlock = (event: React.MouseEvent, type: string) => {
+   event.preventDefault()
+
+   const { editor } = this.props
+   const { value } = editor
+   const { document } = value
+
+   // Handle everything but list buttons.
+   if (type !== 'bulleted-list' && type !== 'numbered-list') {
+     console.log(type)
+    //  if (type === 'heading') {
+    //     const header1Type = 'heading-one'
+    //     const header1 = this.hasBlock(header1Type)
+    //     const header2Type = 'heading-two'
+    //     const header2 = this.hasBlock(header2Type)
+    //     if (header2) {
+    //       editor.setBlocks(header1Type)
+    //     } else if (header1) {
+    //       editor.setBlocks('paragraph')
+    //     } else {
+    //       editor.setBlocks(header2Type)
+    //     }
+    //  }
+     const isActive = this.hasBlock(type)
+     editor.setBlocks(isActive ? 'paragraph' : type)
+   } else {
+     // Handle the extra wrapping required for list buttons.
+     const isList = this.hasBlock('list-item')
+     const isType = value.blocks.some((block: any) => {
+       return !!document.getClosest(block.key, (parent: any) => parent.type === type)
+     })
+
+     if (isList && isType) {
+       editor
+         .setBlocks('paragraph')
+         .unwrapBlock('bulleted-list')
+         .unwrapBlock('numbered-list')
+     } else if (isList) {
+       editor
+         .unwrapBlock(
+           type === 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+         )
+         .wrapBlock(type)
+     } else {
+       editor.setBlocks('list-item').wrapBlock(type)
+     }
+   }
+ }
+
+ hasBlock = (type: string) => {
+   const { editor } = this.props
+   const { value } = editor
+   return value.blocks.some((node: any) => node.type === type)
+ }
+ renderBlockButton = (type: string, icon: string) => {
+   let isActive = this.hasBlock(type)
+
+   // TODO: Add bulleted lists support
+   // see: https://github.com/ianstormtaylor/slate/blob/master/examples/rich-text/index.js
+   if (['numbered-list', 'bulleted-list'].includes(type)) {
+     const { editor } = this.props
+     const { value } = editor
+     const { document, blocks } = value
+
+     if (blocks.size > 0) {
+       const parent = document.getParent(blocks.first().key)
+       isActive = this.hasBlock('list-item') && parent && parent.type === type
+     }
+   }
+   
+   return this.getIcon(icon, isActive, (event) => { this.onClickBlock(event, type) })
+ }
+
+ render() {
+   const { innerRef } = this.props
+   const main = window.document.getElementById('main')
+   if (!main) return
+   console.log('Render menu')
+   return ReactDOM.createPortal(
+     <StyledMenu ref={innerRef}>
+       {this.renderBlockButton('heading-one', 'format_heading_one')}
+       {this.renderBlockButton('heading-two', 'format_heading_two')}
+       {this.renderMarkButton('bold', 'format_bold')}
+       {this.renderMarkButton('italic', 'format_italic')}
+       {this.renderMarkButton('underlined', 'format_underlined')}
+       {this.renderMarkButton('code', 'code')}
+       {this.renderBlockButton('numbered-list', 'format_list_numbered')}
+     </StyledMenu>,
+     main as Element
+   )
+ }
+}
+
+export default EditorMenu
---
+++ src/Components/NoteArea.tsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import { Editor, EditorProps, RenderMarkProps, RenderNodeProps } from 'slate-react'
+import { Value } from 'slate'
+import EditorMenu from './EditorMenu'
+
+interface TextAreaProps {
+  value: Value
+  onChange: (note: Value) => void
+  style?: React.CSSProperties
+}
+
+class NoteArea extends React.Component<TextAreaProps> {
+  /**
+   * Deserialize the raw initial value.
+   *
+   * @type {Object}
+   */
+
+  menu?: HTMLDivElement | null
+
+  // state = {
+  //   value: initialValue
+  // }
+  /**
+   * On update, update the menu.
+   */
+
+  componentDidMount = () => {
+    this.updateMenu()
+  }
+
+  componentDidUpdate = () => {
+    this.updateMenu()
+  }
+
+  /**
+   * Update the menu's absolute position.
+   */
+
+  updateMenu = () => {
+    const menu = this.menu
+    if (!menu) return
+
+    const { value } = this.props
+    const { fragment, selection } = value
+
+    // avoids moving the menu after already open
+    if (selection.isBlurred || selection.isCollapsed || fragment.text === '') {
+      menu.removeAttribute('style')
+      return
+    }
+
+    const native = window.getSelection()
+    const range = native && native.getRangeAt(0)
+    const rect = range ? range.getBoundingClientRect() : {top: 10, left: 10, width: 100}
+    menu.style.opacity = '1'
+    menu.style.top = `${rect.top - 60}px` //`${rect.top + window.pageYOffset - 100}px`
+    menu.style.height = `46px`
+    menu.style.left = `${rect.left +
+      window.pageXOffset -
+      menu.offsetWidth / 2 +
+      rect.width / 2}px`
+  }
+
+  /**
+   * Render.
+   *
+   * @return {Element}
+   */
+
+   //@ts-ignore
+  onChange = ({ value }) => {
+    // this.setState({ value })
+    this.props.onChange( value )
+  }
+  render() {
+    return (
+      <div>
+        <Editor
+          autoFocus={true}
+          placeholder="Enter some text..."
+          value={this.props.value}
+          onChange={this.onChange}
+          renderNode={this.renderNode}
+          renderEditor={this.renderEditor}
+          renderMark={this.renderMark}
+          style={this.props.style}
+        />
+      </div>
+    )
+  }
+
+
+  renderNode = (props: RenderNodeProps, editor: any, next: () => any) => {
+    const { attributes, children, node } = props
+    switch (node.type) {
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>
+      case 'numbered-list':
+        return <ol {...attributes} >{children}</ol>
+      case 'list-item':
+        return (
+            <li
+                {...props.attributes}
+            >
+                {props.children}
+            </li>
+        )
+      default:
+        return next()
+    }
+  }
+  /**
+   * Render the editor.
+   *
+   * @param {Object} props
+   * @param {Function} next
+   * @return {Element}
+   */
+
+  renderEditor = (props: EditorProps, editor: any, next: () => any) => {
+    const children = next()
+    return (
+      <React.Fragment>
+        {children}
+        <EditorMenu innerRef={(menu: HTMLDivElement | null) => {
+          this.menu = menu
+        }} editor={editor} />
+      </React.Fragment>
+    )
+  }
+
+  renderMark = (props: RenderMarkProps, editor: any, next: () => any) => {
+    const { children, mark, attributes } = props
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>
+      case 'code':
+        return <code {...attributes}>{children}</code>
+      case 'italic':
+        return <em {...attributes}>{children}</em>
+      case 'underlined':
+        return <u {...attributes}>{children}</u>
+      default:
+        return next()
+    }
+  }
+}
+
+export default NoteArea
